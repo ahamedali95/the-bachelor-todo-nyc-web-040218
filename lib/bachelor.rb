6aaf9c6536a566601ad6ecb1361ac54fad046420
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-23 01:42:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash[:status] == "Winner"
+      return contestant_hash[:name]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
