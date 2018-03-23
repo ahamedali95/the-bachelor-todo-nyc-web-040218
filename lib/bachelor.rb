0ecdeedcab667e26c4fb8de86db074c786917e36
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-23 02:26:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,7 +34,7 @@ def get_occupation(data, hometown)
   data.each do |season, contestants|
     contestants.each do |contestant_hash|
       if contestant_hash["hometown"] == hometown
-        contestant_hash["occupation"]
+        return contestant_hash["occupation"]
       end
     end
   end
