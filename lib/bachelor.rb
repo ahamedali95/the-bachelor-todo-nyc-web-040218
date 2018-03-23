b7ac9ce358d62f5915f3b9527bb18c1a6f6dcd36
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-23 02:31:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,7 +46,7 @@ def get_average_age_for_season(data, season)
   
   data[season].each do |contestant_hash|
     age_sum += contestant_hash["age"].to_f
-    number_of_contestants += 1
+    number_of_contestants += 1.0
   end
   
   age_sum / number_of_contestants
